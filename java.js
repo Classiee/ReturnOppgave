8c7bd01474de8e7538b4229653791705a8ceea39
Classiee <Kristianfratini@gmail.com>
2024-08-19 10:48:29 +0200

    Done.

--- java.js
+++ java.js
@@ -1,4 +1,20 @@
-function createAndShowHtml(color, text, fontSize) {
-    document.getElementById("buttons").innerHTML = '
-    <button style="background-color: ' + color + '; font-size: ' + fontSize + 'px;">' + text + '</button>';
+function createAndShowHtml(color, text, fontSize, border) {
+    document.getElementById("buttons").innerHTML =
+    `<div ${createStyle(color, fontSize, border)}>${text}</div>`;
+}
+
+function createStyle(color, fontSize, border) {
+    return `style="${createBackgroundColor(color)}${createFontSize(fontSize)}${createBorder(border)}"`;
+}
+
+function createBackgroundColor(color) {
+    return `background-color: ${color};`;
+}
+
+function createFontSize(fontSize) {
+    return `font-size: ${fontSize}px;`;
+}
+
+function createBorder(border) {
+    return `border: ${border}px solid;`;
 }
